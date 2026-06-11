ba3f35dfd0dd951f314bbaea40b01ff54b1a0e2c
TejasJainX <jaintejas325@gmail.com>
2026-06-11 17:40:43 +0530

    New Project

---
+++ Fake_headline_generator.py
@@ -0,0 +1,12 @@
+import random
+
+
+sub = ["Shah Rukh Khan", "Salman Khan", "Aamir Khan", "Amitabh Bachchan", "Virat Kohli", "MS Dhoni", "Narendra Modi", "Elon Musk", "Bill Gates", "Mark Zuckerberg",
+       "Taylor Swift", "Cristiano Ronaldo", "Lionel Messi", "MrBeast", "Tom Cruise", "Dwayne Johnson", "Donald Duck", "Batman", "Spider-Man", "SpongeBob"]
+action = ["spotted being a cowboy in", "passed by", "crying over", "got a job in", "wants to go to", "accidentally bought", "got banned from", "started a food fight in", "lost a slipper in", "opened a tea stall in", "challenged a goat in",
+          "became principal of", "was caught dancing in", "is hiding from taxes in", "mistook for their house", "declared war on", "got trapped inside", "tried to sell Wi-Fi in", "adopted 37 pigeons from", "started a rock band in"]
+places = ["Delhi", "Mumbai", "Furfuri Nagar", "India Gate", "the Moon", "Mars", "Antarctica", "Area 51", "the Sahara Desert", "Mount Everest", "a random WhatsApp group",
+          "a Minecraft village", "the backrooms", "Hogwarts", "Gotham City", "Spongebob's Home", "a potato farm", "a haunted railway station", "an abandoned shopping mall", "a secret underground bunker"]
+sentence = (
+    f"\"{random.choice(sub)} {random.choice(action)} {random.choice(places)}\"")
+print(sentence)
--- Rock Paper.py
+++ Rock Paper.py
@@ -1,5 +1,7 @@
 import random
-random_number = random.randint(1,3)
+
+
+random_number = random.randint(1, 3)
 if random_number == 1:
     computer = "Rock"
 elif random_number == 2:
@@ -9,6 +11,14 @@ elif random_number == 3:
 
 while True:
     generator = input("Rock, Paper, Scissors? (r/p/s): ")
+
+    random_number = random.randint(1, 3)
+    if random_number == 1:
+        computer = "Rock"
+    elif random_number == 2:
+        computer = "Paper"
+    elif random_number == 3:
+        computer = "Scissor"
     if generator.lower() != 'r' and generator.lower() != 'p' and generator.lower() != 's':
         print('Invalid choice!')
     elif generator.lower() == 'r':
@@ -16,20 +26,40 @@ while True:
         print(f"Computer choose {computer}")
         if computer == "Rock":
             print('Tie')
-
+            continue
         elif computer == "Paper":
             print('You win')
+            continue
         elif computer == "Scissor":
             print('You lose')
+            continue
+    elif generator.lower() == 'p':
+        print('You chose Rock!')
+        print(f"Computer choose {computer}")
+        if computer == "Rock":
+            print('Tie')
+            continue
+        elif computer == "Paper":
+            print('You win')
+            continue
+        elif computer == "Scissor":
+            print('You lose')
+            continue
+    elif generator.lower() == 's':
+        print('You chose Rock!')
+        print(f"Computer choose {computer}")
+        if computer == "Rock":
+            print('Tie')
+            continue
+        elif computer == "Paper":
+            print('You win')
+            continue
+        elif computer == "Scissor":
+            print('You lose')
+            continue
+
 
 INCOMPLETE
 
 
-
-
-
-
-
-
-
-# REMOVED INLINE SUGGEST TO DO ON MY OWN 
+# REMOVED INLINE SUGGEST TO DO ON MY OWN
